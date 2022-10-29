7199ea4b1d157ee9fa89b077e070757aebd944b9
joherrerac <jo.herrerac@duocuc.cl>
2022-10-29 17:36:46 -0300

    primera subida

---
+++ src/app/pages/game/game.page.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-game',
+  templateUrl: './game.page.html',
+  styleUrls: ['./game.page.scss'],
+})
+export class GamePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  change(id, symbol, icon) {
+    document
+  }
+
+}
